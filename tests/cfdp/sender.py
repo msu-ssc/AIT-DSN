97a342bbc9b7a860299fe0966b7b6a4903c4fff5
David Mayo <dcmayo@moreheadstate.edu>
2025-04-21 11:44:35 -0400

    Get class 1 test case working again

---
+++ tests/cfdp/sender.py
@@ -0,0 +1,84 @@
+# This is a barely modified version of
+# `ait/dsn/bin/ait_cfdp_start_sender.py`
+
+import os
+import sys
+from pathlib import Path
+
+root_dir = Path(__file__).resolve().parent.parent.parent
+print(f"root_dir: {root_dir}")
+sys.path.insert(0, str(root_dir))
+os.environ["AIT_ROOT"] = str(root_dir)
+os.environ["AIT_CONFIG"] = str(root_dir / "tests/cfdp/config.yaml")
+
+import ait.dsn.cfdp
+import gevent
+import traceback
+
+from ait.dsn.cfdp.primitives import TransmissionMode
+import ait.core.log
+
+import common
+
+
+def main(args):
+    from pprint import pprint
+
+    pprint(args.__dict__)
+
+    from pathlib import Path
+    import shutil
+
+    incoming = Path("tests/cfdp/datasink/incoming")
+    outgoing = Path("tests/cfdp/datasink/outgoing")
+    pdusink = Path("tests/cfdp/datasink/pdusink")
+    tempfiles = Path("tests/cfdp/datasink/tempfiles")
+
+    for delete_folder in [incoming, pdusink, tempfiles]:
+        if delete_folder.exists():
+            shutil.rmtree(delete_folder)
+
+    cfdp = ait.dsn.cfdp.CFDP(args.sender_entity)
+
+    outgoing_file = common.create_file(size=args.file_size, path=outgoing / f"{args.file_size:_}.txt", fill=args.file_fill.encode(encoding="ascii"))
+    try:
+        sender_addr = (args.sender_host, args.sender_port)
+        receiver_addr = (args.receiver_host, args.receiver_port)
+        cfdp.connect(sender_addr, send_host=receiver_addr)
+
+        destination_id = args.receiver_entity
+        # source_file = args.sender_file
+        source_file = outgoing_file.name
+        # destination_file: str = args.receiver_file
+        destination_file: str = outgoing_file.name
+
+        for rep in range(args.reps):
+            this_destination_file = destination_file.split(".")[0] + f".{str(rep).zfill(5)}" + "." + destination_file.split(".")[1]
+            print(f"Sending {source_file} to {destination_id} as {this_destination_file}")
+            # destination_file = destination_file.split('.')[0] + f'.{str(rep).zfill(5)}' + '.' + destination_file.split('.')[1]
+            cfdp.put(destination_id, source_file, this_destination_file, transmission_mode=TransmissionMode.NO_ACK)
+        while True:
+            # ait.core.log.info('Sleeping...')
+            gevent.sleep(1)
+    except KeyboardInterrupt:
+        print("Disconnecting...")
+    except Exception:
+        print(traceback.print_exc())
+    finally:
+        cfdp.disconnect()
+
+
+if __name__ == "__main__":
+    args = common.parse_args()
+    if args.profile:
+        from pstats import Stats
+        from cProfile import Profile
+
+        with Profile() as profile:
+            main(args)
+            stats = Stats(profile)
+            stats.sort_stats("cumulative")
+            stats.print_stats(50)
+            stats.dump_stats("cfdp_sender_profile.prof")
+    else:
+        main(args)
